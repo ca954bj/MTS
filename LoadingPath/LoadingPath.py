880b484cd22234f5039a32d021c3b4570d63c726
ca954bj <ca954bj@gmail.com>
2019-10-05 22:36:22 +1000

    191005

---
+++ LoadingPath/LoadingPath.py
@@ -0,0 +1,33 @@
+execfile('../MTSsetup.py')
+
+drifts = [0.375, 0.5, 0.75, 1, 1.5, 2, 3, 4, 5, 6]
+cycles = [6, 6, 6, 4, 2, 2, 2, 2, 2, 2]
+
+y = []
+x = []
+count = 0
+
+for i, obj in enumerate(drifts):
+	for j in range(0, cycles[i]):
+		y.append(0)
+		y.append(obj)
+		y.append(0)
+		y.append(-obj)
+		x.append(count)
+		x.append(count + 0.25)
+		x.append(count + 0.5)
+		x.append(count + 0.75)
+		count += 1
+
+print(count)
+plt.figure(figsize=(6, 6))
+ax1 = plt.subplot(1, 1, 1)
+plt.plot(x, y, color='k', linestyle='-')
+plt.ylim(-7, 7)
+plt.xlim(0, 35)
+plt.grid(linestyle='--')
+plt.xlabel('Cycle', fontproperties=fontprop)
+plt.ylabel('Equivalent Story Drift Angle (%)', fontproperties=fontprop)
+
+plt.subplots_adjust(left=0.1, right=0.98, wspace=0.22, hspace=0.22, bottom=0.12, top=0.95)
+plt.show()
